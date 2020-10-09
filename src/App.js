9383a9a259d04b5e0068541523be785f6253c092
Sebastien Lajeunesse-deGroot <sebastiendegroot2@gmail.com>
2020-10-09 01:00:53 -0700

    time v1 (doesn't work at all)

--- src/App.js
+++ src/App.js
@@ -2,18 +2,43 @@ import React from 'react';
 import './App.css';
 import TypingBox from './components/TypingBox'; 
 
+const gameLength = 60; 
+var interval;
 
-function App() {
-  return (
-    <div className = "App">
-      <header className = "App-header">
-        <TypingBox></TypingBox>
-      </header>
-       
-    </div>
-   
-  )
+var gameState = {
+  time: gameLength,
+  inGame: true
 }
+
+const startGame = () => {
+
+    interval = setInterval(() => {
+    gameState.time--; 
+
+    if (gameState.time <= 0) {
+      gameState.inGame = false; 
+      clearInterval(interval);
+    }
+  },1000);
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    startGame(); 
+  }
+  render() {
+    return (
+      <div className = "App">
+      <header className = "App-header">
+        <h1 className = "mb-5">{gameState.time} seconds left</h1>
+        <TypingBox></TypingBox>
+      </header> 
+    </div>
+    )
+  }
+}
+
 
 
 export default App;
