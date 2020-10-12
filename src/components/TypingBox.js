57a90d73e36619022f2b0cb8c237793419ff3d7b
Sebastien Lajeunesse-deGroot <sebastiendegroot2@gmail.com>
2020-10-11 22:50:24 -0700

    feature/Fables

--- src/components/TypingBox.js
+++ src/components/TypingBox.js
@@ -1,4 +1,4 @@
-import React from "react"; 
+import React, { Fragment } from "react"; 
 import "./TypingBox.css"; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from "react-bootstrap/Form";
@@ -7,10 +7,14 @@ import { Button, Container } from "react-bootstrap";
 import ColoredText from "./ColoredText.js";
 import {FairyTales} from "./FairyTales.js";
 
+
 class TypingBox extends React.Component {
 
     //text = "This is what the player will have to type really quickly so that they can look really cool and impress all of their friends by having a word per minute speed that is litteraly faster than the speed of light, do you dig it? ";
     
+    getRandomInt = (max) => {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
 
     initialState = {
         gameState: 0,
@@ -21,18 +25,19 @@ class TypingBox extends React.Component {
         spellCheck: "success", 
         textInput: {},
         time: 10,
+        textObject: FairyTales[this.getRandomInt(FairyTales.length)]
     }
 
     constructor(props) {
         super(props);
-
-        console.log(FairyTales[0])
         this.state = this.initialState;
         this.coloredText = [];
-        for(let i = 0; i< this.state.textArry.length; i++){
-            this.coloredText.push({word:this.state.textArry[i], color:"#FF00FF"}); 
+        var initArry = this.state.textObject.text.split(" "); 
+        for(let i = 0; i < initArry.length; i++) {
+            this.coloredText.push({word: initArry[i], color: "#000000"});
         }
     }
+
 
     getErrors = (textInput, textArry) => {
         let currErrors = 0
@@ -45,12 +50,11 @@ class TypingBox extends React.Component {
     }
 
     checkText = (textInput, textArry) => {
-        console.log(textInput);
         for(let i = 0; i < textArry.length - 1; i++) {
             if(textInput[i]) {
                 if ((textInput[i] !== textArry[i])) {
                     if (i === textInput.length - 1) {
-                        this.coloredText[i].color = "#FFFF00";
+                        this.coloredText[i].color = "#F9B847";
                     } else {
                         this.coloredText[i].color = "#FF0000";
                     }
@@ -68,9 +72,7 @@ class TypingBox extends React.Component {
     interval = null;
 
     handleTextInput = (event) => {
-      
-        this.coloredText[0].color = "#00FF00";
-
+    
         if (!this.interval) {
             this.interval = setInterval(() => {
            
@@ -92,12 +94,33 @@ class TypingBox extends React.Component {
             wordIndex: event.target.value.split(" ").length - 1,
             currentChar: event.target.value.split(" ").pop().slice(-1),
             charIndex: event.target.value.split(" ").pop().length,
-            spellCheck: (this.text.split(" ")[event.target.value.split(/ +/).length - 1].substring(0, event.target.value.split(" ").pop().length) === event.target.value.split(" ").pop()) ? "success":"danger"
+            spellCheck: (this.state.textObject.text.split(" ")[event.target.value.split(/ +/).length - 1].substring(0, event.target.value.split(" ").pop().length) === event.target.value.split(" ").pop()) ? "success":"danger"
         })
     }
     
     handleButtonClick = (event) => {
-        this.setState(this.initialState);
+
+        var randObj = FairyTales[this.getRandomInt(FairyTales.length)];
+    
+        this.setState({
+            gameState: 0,
+            currentWord: 0, 
+            wordIndex: 0,
+            currentChar: 0,
+            charIndex: 0,
+            spellCheck: "success", 
+            textInput: {},
+            time: 10,
+            textObject: randObj
+        });
+        var currArry = randObj.text.split(" "); 
+        for(let i = 0; i < currArry.length; i++) {
+            if(!this.coloredText[i]) {
+                this.coloredText.push({word: currArry[i], color: "#000000"});
+            }
+            this.coloredText[i] = {word: currArry[i], color: "#000000"};
+        }
+
     }
 
     render = () => {
@@ -105,15 +128,17 @@ class TypingBox extends React.Component {
             return (                   
                 <Container>
                     <h1 className = "mb-5">{this.state.time} seconds left</h1>
-                    <Alert variant={this.state.spellCheck} className = "mb-5">
-                        <ColoredText coloredText={this.checkText(this.state.textInput, this.state.textArry)}></ColoredText>
+                    <Alert variant={this.state.spellCheck} >
+                        <ColoredText coloredText={this.checkText(this.state.textInput, this.state.textObject.text.split(" "))}></ColoredText>
                     </Alert>
+                    <h2 className = "mb-5">{this.state.textObject.name}</h2>
                     <Form>
                         <Form.Group>
                             <Form.Control type = "textarea" placeholder = "Type here!" onChange={this.handleTextInput} />
                             <Form.Text className = "text-muted">
                                 You will have 60 seconds once the test begins.
                             </Form.Text>
+                            
                         </Form.Group>
                     </Form>
                     
@@ -125,7 +150,7 @@ class TypingBox extends React.Component {
                 <Button onClick={this.handleButtonClick} className = "mb-3">Click to play again!</Button>
                 <Container>
                     <p>WMP: {(this.state.textInput.length - 1) / (this.initialState.time / 60)}</p>
-                    <p>Accuracy: {((this.state.textInput.length - 1 - this.getErrors(this.state.textInput, this.state.textArry)) / (this.state.textInput.length - 1) * 100).toFixed(2)} %</p>
+                    <p>Accuracy: {((this.state.textInput.length - 1 - this.getErrors(this.state.textInput, this.state.textObject.text.split(" "))) / (this.state.textInput.length - 1) * 100).toFixed(2)} %</p>
                 </Container>
                 
             </div>         
